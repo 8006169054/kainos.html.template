5bc8ec362b239dc4a739075215663135d82cf3ff
8006169054 <43157353+8006169054@users.noreply.github.com>
2025-03-21 15:18:03 +0900

    alert message 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -3,4 +3,36 @@ loding = async (type) => {
 		$("#loading").show();
 	else
 		$("#loading").hide();
-}
+};
+
+/**
+ * type = success , info, warning, error
+ */
+alertMessage = async (message, type, title) => {
+	if(title === undefined){
+		if(type === 'success') title = 'Success';
+		else if(type === 'info') title = 'Info';
+		else if(type === 'warning') title = 'Warning';
+		else if(type === 'error') title = 'Error';
+	}
+	swal(title, message, type);
+};
+
+/**
+ * type = success , info, warning, error
+ */
+confirmMessage = async (message, type, title, callFn) => {
+	swal({
+      title: title,
+      text: message,
+      icon: type,
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((selection) => {
+      if (selection)
+      	callFn(selection);
+      else
+      	callFn(false);
+    });
+};
