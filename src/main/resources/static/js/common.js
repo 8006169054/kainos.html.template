d1809f1cbc3f420666c84e6eff8ca79bf27ad777
8006169054 <43157353+8006169054@users.noreply.github.com>
2025-03-24 17:32:08 +0900

    다국어 처리 기능 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -44,3 +44,39 @@ dataBinding = async (bindingData, prefix = "") => {
 		$("#" + prefix + key).val(bindingData[key]);
 	});
 };
+
+/**
+ * 
+ */
+setCookie = (name, value) => {
+	document.cookie = name + "=" + escape( value ) + "; path=/;";
+}
+    
+/**
+ * 
+ */
+getCookie = (name) => {
+	var Found = false; 
+	var start, end; 
+	var i = 0;
+
+	while(i <= document.cookie.length) { 
+		start = i; 
+		end = start + name.length; 
+		    
+		if(document.cookie.substring(start, end) == name) { 
+		   Found = true; 
+		   break; 
+		} 
+		i++; 
+	} 
+		    
+	if(Found == true) { 
+		start = end + 1; 
+		end = document.cookie.indexOf(";", start); 
+		if(end < start) 
+			end = document.cookie.length; 
+		return document.cookie.substring(start, end); 
+	} 
+	return "";	  
+}
